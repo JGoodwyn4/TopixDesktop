93bd416ab0700af6a9eefc1b63472fde70eb77f8
John Goodwyn <jgoodwyn4@gmail.com>
2018-03-29 01:42:59 -0400

    Navigation bar implemented

--- TopixApp/MainWindow.xaml.cs
+++ TopixApp/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -27,9 +28,73 @@ namespace TopixApp
             ContentDisplay.Content = new UserProfile();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void OpenMenu_Click(object sender, RoutedEventArgs e)
         {
-            
+            ToggleMenu("slideOut");
+        }
+
+        private void CloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleMenu("slideIn");
+        }
+
+        private void ProfileNav_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HubNav_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TopixNav_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EventNav_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchNav_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MessageNav_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SettingsNav_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LogoutNav_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToggleMenu(string sbType)
+        {
+            // Code based on http://www.codescratcher.com/wpf/sliding-panel-in-wpf/
+
+            Storyboard sb = Resources[sbType] as Storyboard;
+            sb.Begin(navBar);
+
+            if(sbType.Equals("slideOut"))
+            {
+                OpenButton.Visibility = Visibility.Hidden;
+                CloseButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                OpenButton.Visibility = Visibility.Visible;
+                CloseButton.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
